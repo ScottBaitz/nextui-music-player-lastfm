23aa6ab50f39cfd9266f483a69203762c45a4cf6
mohammadsyuhada <mohammadsyuhada@gmail.com>
2026-01-13 23:59:36 +0800

    fix: increase CPU max frequency from 600 MHz to 1000 MHz

--- launch.sh
+++ launch.sh
@@ -5,10 +5,10 @@ export LD_LIBRARY_PATH="$DIR:$DIR/bins:$LD_LIBRARY_PATH:/usr/bin"
 # Set HOME so ALSA can find .asoundrc for Bluetooth/USB DAC audio routing
 export HOME="/mnt/SDCARD/.userdata/tg5040"
 
-# Set CPU frequency for music player (power saving: 408-600 MHz)
+# Set CPU frequency for music player (power saving: 408-1000 MHz)
 echo ondemand > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 echo 408000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-echo 600000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+echo 1000000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 
 # Run the app
 "$DIR/musicplayer.elf" &> "$DIR/log.txt"
